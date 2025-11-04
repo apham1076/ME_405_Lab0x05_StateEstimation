b26e3693b7ff8358f3525d02badb1db7bb8433af
mduperly <mikeyduperly@gmail.com>
2025-11-04 05:12:28 -0800

    replaced with more updated file from updated lab 0x03

--- encoder.py
+++ encoder.py
@@ -2,7 +2,13 @@ from pyb import Pin, Timer
 from time import ticks_us, ticks_diff   # Use to get dt value in update()
 
 class Encoder:
-    '''A quadrature encoder decoding interface encapsulated in a Python class'''
+    '''A quadrature encoder decoding interface encapsulated in a Python class. Provides position in [rad] and velocity in [rad/s].'''
+
+    # --------------------------------------------------------------------------
+    # CONSTANTS
+    CPR = 1440            # Counts (pulses) per revolution of the encoder
+    RAD_PER_COUNT = 2*3.14159265 / CPR  # Radians per count
+    # --------------------------------------------------------------------------
 
     def __init__(self,
                  tim_num: int,
@@ -16,56 +22,53 @@ class Encoder:
         self.tim.channel(1, mode=Timer.ENC_AB, pin=self.chA_pin)
         self.tim.channel(2, mode=Timer.ENC_AB, pin=self.chB_pin)
 
-        self.position   = 0     # Total accumulated position of the encoder
+        # Internal states
+        self.position_counts   = 0     # Total accumulated position counts
         self.prev_count = self.tim.counter() # initialize from current counter
-        self.delta      = 0     # Change in count between last two updates
-        self.prev_time  = ticks_us()    # Time from most recent update
+        self.delta = 0      # Change in count between last two updates
+        self.prev_time = ticks_us()    # Time from most recent update
         self.dt         = 0     # Amount of time between last two updates
+        self.velocity_counts_per_s = 0  # Velocity in counts per second
         
-    
+    # --------------------------------------------------------------------------
     def update(self):
-        '''Runs one update step on the encoder's timer counter to keep
-           track of the change in count and check for counter reload'''
+        '''Update encoder count and compute velocity.'''
         curr_count = self.tim.counter()
         delta = curr_count - self.prev_count
 
-        # Check if delta is out of range
+        # Check if delta is out of range (handle 16-bit over/underflow)
         if delta < -(self.AR + 1)/2:
             delta += self.AR + 1
         elif delta > (self.AR + 1)/2:
             delta -= self.AR + 1
         
-        # Determine dt
-        self.position += delta
+        # Update count-based position
+        self.position_counts += delta
         self.delta = delta
+
+        # Compute time step and velocity
         curr_time = ticks_us()
         self.dt = ticks_diff(curr_time, self.prev_time)
         if self.dt != 0:
-            self.velocity = self.delta*1e6 / self.dt
+            self.velocity_counts_per_s = self.delta*1e6 / self.dt
         else:
-            self.velocity = 0
+            self.velocity_counts_per_s = 0
 
-        self.position = self.position*2*3.14159265/1440  # position in [rad]
-        self.velocity = self.velocity*2*3.14159265/1440  # velocity in [rad/s]
-
-        self.prev_time = curr_time
+        # Save for next update
         self.prev_count = curr_count
-            
+        self.prev_time = curr_time
+
+    # --------------------------------------------------------------------------
     def get_position(self):
-        '''Returns the most recently updated value of position as determined
-           within the update() method'''
-        return self.position  # position in [rad]
-            
+        '''Return position in radians.'''
+        return self.position_counts * self.RAD_PER_COUNT  # position in [rad]
+
     def get_velocity(self):
-        '''Returns a measure of velocity using the the most recently updated
-           value of delta as determined within the update() method'''
-        return self.velocity  # velocity in [rad/s]
-        # return self.delta*1e6 // self.dt if self.dt !=0 else 0
-               # velocity in [encoder counts/second]
-    
+        '''Return velocity in radians per second.'''
+        return self.velocity_counts_per_s * self.RAD_PER_COUNT  # velocity in [rad/s]
+
     def zero(self):
-        '''Sets the present encoder position to zero and causes future updates
-           to measure with respect to the new zero position'''
-        self.position = 0
+        '''Zero the encoder position.'''
+        self.position_counts = 0
         self.prev_count = self.tim.counter()
         self.prev_time = ticks_us()
